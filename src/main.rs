13da56bc1d6e75e6de21df45db0e156d7ff90c72
bayge <alex@fluidity.money>
2024-09-22 00:26:48 +0930

    Flip storage word to uint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-#![no_main]
-#![no_std]
+#![cfg_attr(target_arch = "wasm32", no_main, no_std)]
 
 use stylus_sdk::{
     alloy_primitives::*,
@@ -17,12 +16,12 @@ use hex_literal::hex;
 extern crate alloc;
 
 //bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
-const STORAGE_LOGIC: U256 = U256::from_le_bytes(hex!(
+const STORAGE_LOGIC: U256 = U256::from_be_bytes(hex!(
     "360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
 ));
 
 //bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1)
-const STORAGE_ADMIN: U256 = U256::from_le_bytes(hex!(
+const STORAGE_ADMIN: U256 = U256::from_be_bytes(hex!(
     "b53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103"
 ));
 
@@ -65,4 +64,9 @@ pub extern "C" fn user_entrypoint(len: usize) -> usize {
         contract::output(&d);
         s
     }
+}
+
+#[test]
+fn print_storage() {
+    dbg!(STORAGE_LOGIC, STORAGE_ADMIN);
 }
